8b5740841b16ac4c7fede27b19a1b72c985dfd74
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-07-04 21:43:16 +0200

    Quest 8 achieved

---
+++ Eagle.java
@@ -0,0 +1,67 @@
+public class Eagle extends Bird implements Fly{
+
+    private boolean flying;
+    private int altitude;
+
+    public Eagle(String name) {
+        super(name);
+        this.flying = false;
+        this.altitude = 0;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public boolean isFlying() {
+        return flying;
+    }
+
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+
+    @Override 
+    public void takeOff() {
+        if(!this.isFlying() && this.getAltitude() == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky.");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if(this.isFlying()) {
+            this.altitude = Math.min(this.altitude + meters, 150);
+            System.out.println(this.getName() + " flies upward, altitude : " + this.getAltitude());
+        }
+        return this.altitude;
+    }
+
+    @Override
+
+    public int descend(int meters) {
+        if(this.isFlying()) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(this.getName() + " flies downward, altitude : " + this.getAltitude());
+        }
+        return this.altitude;
+    }
+
+    @Override
+
+    public void glide() {
+        System.out.println(this.getName() + "  glides into the air.");
+    }
+
+    public void land() {
+        if (this.isFlying() && this.altitude <= 1 ){
+            this.isFlying();
+            this.altitude = 0;
+            System.out.println(this.getName() + " lands on the ground");
+        } else {
+            System.out.println(this.getName() + " is not crazy,  it's too high");
+        }
+    }
+}
